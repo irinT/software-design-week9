27e8c4db839b0d7e7c3bcc7a6ae1e711061b0cab
Brian Eng <brian@brianeng.com>
2020-02-26 16:14:29 +0000

    week 9 start

---
+++ app.rb
@@ -0,0 +1,34 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sinatra"                                                                     #
+require "sinatra/reloader" if development?                                            #
+require "sinatra/reloader" if development?                                            #
+require "sinatra/reloader" if development?                                            #
+require "sequel"                                                                      #
+require "logger"                                                                      #
+require "twilio-ruby"                                                                 #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
+def view(template); erb template.to_sym; end                                          #
+use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'           #
+before { puts; puts "--------------- NEW REQUEST ---------------"; puts }             #
+after { puts; }                                                                       #
+#######################################################################################
+
+events_table = DB.from(:events)
+rsvps_table = DB.from(:rsvps)
+
+get "/" do
+    puts events_table.all
+    @events = events_table.all
+    view "events"
+end
+
+get "/events/:id" do
+    @event = events_table.where(id: params[:id]).first
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(id: params[:id]).first
+    view "new_rsvp"
+end
